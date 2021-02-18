418090199a54da5b7747d1361b0013db525b5411
caitlin halphen <chalph3@lsu.edu>
2021-02-18 17:25:56 -0600

    commit t maain

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,12 +1,8 @@
 # tweets/urls.py
 from django.urls import path
-# from .views import TweetListView, TweetCreateView
-from . import views
+from .views import TweetListView, TweetCreateView
 
 urlpatterns = [
-    path('tweets/new/', views.tweet_new, name='tweet_new'),
-    path('', views.tweet_list, name='home'),
-
-    # path('tweets/new/', TweetCreateView.as_view(), name='tweet_new'),
-    # path('', TweetListView.as_view(), name='home'),
+    path('tweets/new/', TweetCreateView.as_view(), name='tweet_new'),
+    path('', TweetListView.as_view(), name='home'),
 ]
--- users/views.py
+++ users/views.py
@@ -4,19 +4,16 @@ from django.views import generic
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 from django.contrib.auth.views import PasswordChangeView, PasswordResetView
-from django.contrib.messages.views import SuccessMessageMixin # new
 
-class SignUpView(SuccessMessageMixin, generic.CreateView): # new
+class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
-    success_message = 'Account was created successfully.' # new
 
-class UserUpdateView(SuccessMessageMixin, generic.UpdateView): # new
+class UserUpdateView(generic.UpdateView):
     form_class = CustomUserChangeForm
     success_url = reverse_lazy('home')
     template_name = 'update.html'
-    success_message = 'User profile updated.' # new
 
     # This keeps users from accessing the profile of other users.
     def get_queryset(self):
@@ -26,12 +23,10 @@ class UserUpdateView(SuccessMessageMixin, generic.UpdateView): # new
         else:
             return CustomUser.objects.filter(id=user.id)
 
-class UserPasswordChangeView(SuccessMessageMixin, PasswordChangeView): # new
+class UserPasswordChangeView(PasswordChangeView):
     success_url = reverse_lazy('home')
     template_name = 'change_password.html'
-    success_message = 'Password changed.' # new
 
-class UserPasswordResetView(SuccessMessageMixin, PasswordResetView): # new
+class UserPasswordResetView(PasswordResetView):
     success_url = reverse_lazy('login')
     template_name = 'reset_password.html'
-    success_message = 'Check your email for a reset link.' # new
